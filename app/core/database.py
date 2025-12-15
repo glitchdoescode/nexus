685c40ed8c483ad845592d27e42dac09fa3d43ab
0xglitch <deepakkrsoni16@gmail.com>
2025-12-15 12:38:20 +0530

    some restructuring

--- app/core/database.py
+++ app/core/database.py
@@ -5,7 +5,12 @@ from app.core.config import settings
 
 engine = create_async_engine(settings.database_url, echo=True)
 
-async_session_factory = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False, autoflush=False)
+async_session_factory = async_sessionmaker(
+    bind=engine, 
+    class_=AsyncSession, 
+    expire_on_commit=False, 
+    autoflush=False
+    )
 
 async def getdb() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_factory() as session:
